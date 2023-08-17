873b9d120349103297fa9d95097e7d28ef2fa32b
Niharika <debniharika3@gmail.com>
2023-08-17 13:36:08 +0530

    commit 3

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 englishButton.addEventListener("click", function (e) {
   e.preventDefault();
 });*/
+
+const screenHeight = window.innerHeight;
+const screenHeightPercentage = (screenHeight / window.screen.height) * 100;
+console.log(`Screen height: ${screenHeightPercentage}%`);
